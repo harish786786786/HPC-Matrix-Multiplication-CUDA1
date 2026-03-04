1f6caf9422def83e49ca6bc9ffe7c13cc4994b98
harish786786786 <harish25120013@snuchennai.edu.in>
2026-03-04 16:52:16 +0530

    Add files via upload

---
+++ matrix_mult.cu
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+#define N 2
+
+// -----------------------------
+// Kernel Function (GPU)
+// -----------------------------
+__global__ void matrixMul(int *A, int *B, int *C)
+{
+    int row = threadIdx.y;
+    int col = threadIdx.x;
+
+    int sum = 0;
+
+    for(int k=0;k<N;k++)
+    {
+        sum += A[row*N + k] * B[k*N + col];
+    }
+
+    C[row*N + col] = sum;
+}
+
+// -----------------------------
+// Host Function (CPU)
+// -----------------------------
+int main()
+{
+    int A[N][N] = {{1,2},{3,4}};
+    int B[N][N] = {{5,6},{7,8}};
+    int C[N][N];
+
+    int *d_A,*d_B,*d_C;
+
+    int size = N*N*sizeof(int);
+
+    cudaMalloc((void**)&d_A,size);
+    cudaMalloc((void**)&d_B,size);
+    cudaMalloc((void**)&d_C,size);
+
+    cudaMemcpy(d_A,A,size,cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B,B,size,cudaMemcpyHostToDevice);
+
+    dim3 threads(N,N);
+
+    matrixMul<<<1,threads>>>(d_A,d_B,d_C);
+
+    cudaMemcpy(C,d_C,size,cudaMemcpyDeviceToHost);
+
+    printf("Matrix Multiplication Result:\n");
+
+    for(int i=0;i<N;i++)
+    {
+        for(int j=0;j<N;j++)
+        printf("%d ",C[i][j]);
+
+        printf("\n");
+    }
+
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
+
+    return 0;
+}
